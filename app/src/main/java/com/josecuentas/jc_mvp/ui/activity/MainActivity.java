a22c52147919ee38ce19353cf417c8e548ba116e
José Cuentas Turpo <jcuentast@gmail.com>
2015-11-27 11:15:55 -0500

    Initial commit: configuration initial (.gradle, dependencies.gradle, packages)

---
+++ app/src/main/java/com/josecuentas/jc_mvp/ui/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.josecuentas.jc_mvp.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.josecuentas.jc_mvp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
